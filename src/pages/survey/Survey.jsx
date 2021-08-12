b44c93abe55926869aa54737820b4a32ae176ebd
smb <smb>
2021-08-12 16:45:52 +0900

    added dynamic generation of survey questions

--- src/pages/survey/Survey.jsx
+++ src/pages/survey/Survey.jsx
@@ -11,77 +11,103 @@ import SurveyCheckboxElement from "./SurveyCheckboxElement";
 import SurveyRating2Element from "./SurveyRating2Element";
 import SurveyRating5Element from "./SurveyRating5Element";
 import SurveyRating10Element from "./SurveyRating10Element";
+import { object } from "prop-types";
 
 const SurveyPage = () => {
-  const textOptions = {
-    qNum: 1,
-    type: "text",
-    mandatory: true,
-    label: "Age*",
-    note: "Please enter your year of birth",
-    limitLength: true,
-    maxLen: 4,
-    numsOnly: true,
-  };
+  const surveyQuestionObjects = [
+    {
+      qNum: 1,
+      type: "text",
+      mandatory: true,
+      label: "Age*",
+      note: "Please enter your year of birth",
+      limitLength: true,
+      maxLen: 4,
+      numsOnly: true,
+    },
+    {
+      qNum: 2,
+      type: "textArea",
+      mandatory: true,
+      label: "Comments",
+    },
+    {
+      qNum: 3,
+      type: "radio",
+      mandatory: true,
+      label: "Year*",
+      note: "Please select your year",
+      options: "Freshman; Sophomore; Junior; Senior;",
+    },
+    {
+      qNum: 4,
+      type: "select",
+      mandatory: true,
+      label: "What is your program focus?",
+      options: "Global Studies; Linguistics; English Literature",
+    },
+    {
+      qNum: 5,
+      type: "checkbox",
+      mandatory: true,
+      label: "What type of class do you prefer?",
+      options: "Lecture; Group Discussion; Active Learning",
+    },
+    {
+      qNum: 6,
+      type: "rating2",
+      mandatory: true,
+      label: "Please answer the following questions",
+      scale: "Yes; No",
+      options:
+        "I have used an iPad in class before.; I have used a notebook computer in class before.",
+    },
+    {
+      qNum: 7,
+      type: "rating5",
+      mandatory: true,
+      label: "Please answer the following questions.",
+      options:
+        "How would you rate the use of iPads in this class?; How would you rate this class overall",
+    },
+    {
+      qNum: 8,
+      type: "rating10",
+      mandatory: true,
+      label: "Please answer the following questions.",
+      options:
+        "How would you rate the use of the Socrative website in this class?; How would you rate the use of the Quizlet website in this class?",
+    },
+  ];
 
-  const textAreaOptions = {
-    qNum: 2,
-    type: "textArea",
-    mandatory: true,
-    label: "Comments",
-  };
-
-  const radioOptions = {
-    qNum: 3,
-    type: "radio",
-    mandatory: true,
-    label: "Year*",
-    note: "Please select your year",
-    options: "Freshman; Sophomore; Junior; Senior;",
-  };
-
-  const dropdownOptions = {
-    qNum: 4,
-    type: "select",
-    mandatory: true,
-    label: "What is your program focus?",
-    options: "Global Studies; Linguistics; English Literature",
-  };
-
-  const checkboxOptions = {
-    qNum: 5,
-    type: "checkbox",
-    mandatory: true,
-    label: "What type of class do you prefer?",
-    options: "Lecture; Group Discussion; Active Learning",
-  };
-
-  const rating2Options = {
-    qNum: 6,
-    type: "rating2",
-    mandatory: true,
-    label: "Please answer the following questions",
-    scale: "Yes; No",
-    options:
-      "I have used an iPad in class before.; I have used a notebook computer in class before.",
-  };
-
-  const rating5Options = {
-    qNum: 7,
-    type: "rating5",
-    mandatory: true,
-    label: "Please answer the following questions.",
-    options:
-      "How would you rate the use of iPads in this class?; How would you rate this class overall",
-  };
-
-  const rating10Options = {
-    qNum: 8,
-    type: "rating10",
-    mandatory: true,
-    label: "Please answer the following questions.",
-    options:
-      "How would you rate the use of the Socrative website in this class?; How would you rate the use of the Quizlet website in this class?",
+  const SurveyQuestions = () => {
+    const QuestionList = surveyQuestionObjects.map((object, index) => {
+      if (object.type === "text") {
+        return <SurveyTextElement opts={object} />;
+      }
+      if (object.type === "textArea") {
+        return <SurveyTextAreaElement opts={object} />;
+      }
+      if (object.type === "radio") {
+        return <SurveyRadioElement opts={object} />;
+      }
+      if (object.type === "select") {
+        return <SurveyDropdownElement opts={object} />;
+      }
+      if (object.type === "checkbox") {
+        return <SurveyCheckboxElement opts={object} />;
+      }
+      if (object.type === "rating2") {
+        return <SurveyRating2Element opts={object} />;
+      }
+      if (object.type === "rating5") {
+        return <SurveyRating5Element opts={object} />;
+      }
+      if (object.type === "rating10") {
+        return <SurveyRating10Element opts={object} />;
+      }
+    });
+    return QuestionList;
   };
 
   setTimeout(function () {
@@ -92,14 +118,7 @@ const SurveyPage = () => {
   return (
     <Container>
       <h1>Survey Page!</h1>
-      <SurveyTextElement opts={textOptions} />
-      <SurveyTextAreaElement opts={textAreaOptions} />
-      <SurveyRadioElement opts={radioOptions} />
-      <SurveyDropdownElement opts={dropdownOptions} />
-      <SurveyCheckboxElement opts={checkboxOptions} />
-      <SurveyRating2Element opts={rating2Options} />
-      <SurveyRating5Element opts={rating5Options} />
-      <SurveyRating10Element opts={rating10Options} />
+      <SurveyQuestions />
     </Container>
   );
 };
