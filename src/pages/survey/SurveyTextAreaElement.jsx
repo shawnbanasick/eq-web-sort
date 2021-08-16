865e3216deb2835fdac8f67c25e46f48aba5574a
smb <smb>
2021-08-16 11:28:51 +0900

    added persistence to textArea component

--- src/pages/survey/SurveyTextAreaElement.jsx
+++ src/pages/survey/SurveyTextAreaElement.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { view } from "@risingstack/react-easy-state";
 
 const SurveyTextAreaElement = (props) => {
+  let savedTextAreaText;
+  const [userText, setUserText] = useLocalStorage(
+    "savedTextAreaText",
+    savedTextAreaText
+  );
+
   const handleOnChange = (e) => {
-    console.log(`qNum${props.opts.qNum}-${props.opts.type}`, e.target.value);
+    let value = e.target.value;
+    setUserText(value);
+
+    console.log(`qNum${props.opts.qNum}-${props.opts.type}`, value);
   };
 
   return (
     <Container>
       <TitleBar>{props.opts.label}</TitleBar>
-      <TextAreaInput onChange={handleOnChange} />
+      <TextAreaInput value={userText || ""} onChange={handleOnChange} />
     </Container>
   );
+
+  function useLocalStorage(key, initialValue) {
+    // State to store our value
+    // Pass initial state function to useState so logic is only executed once
+    const [storedValue, setStoredValue] = useState(() => {
+      try {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        // If error also return initialValue
+        console.log(error);
+        return initialValue;
+      }
+    });
+    // Return a wrapped version of useState's setter function that ...
+    // ... persists the new value to localStorage.
+    const setValue = (value) => {
+      try {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        // Save state
+        setStoredValue(valueToStore);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        // A more advanced implementation would handle the error case
+        console.log(error);
+      }
+    };
+    return [storedValue, setValue];
+  }
 };
 
 export default view(SurveyTextAreaElement);
@@ -51,4 +94,5 @@ const TextAreaInput = styled.textarea`
   width: 100%;
   border-radius: 3px;
   border: 2px solid lightgray;
+  padding: 5px;
 `;
