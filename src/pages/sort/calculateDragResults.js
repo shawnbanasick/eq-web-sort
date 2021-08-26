ee873b677661512b4e0768fc6054438cac799e73
Shawn Banasick <banasick@gmail.com>
2021-08-26 13:06:13 +0900

    added results reporting to grid sort

--- src/pages/sort/calculateDragResults.js
+++ src/pages/sort/calculateDragResults.js
@@ -3,26 +3,47 @@ import setGlobalState from "../../globalState/setGlobalState";
 
 const calculateDragResults = (result) => {
   try {
-    // console.log(result);
+    // const sortGridResults = getGlobalState("sortGridResults");
+    const configObj = getGlobalState("configObj");
+    const results = getGlobalState("results");
+    const totalStatements = configObj.totalStatements;
+    const sortGridResults =
+      JSON.parse(localStorage.getItem("sortGridResults")) || {};
 
-    const sortGridResults = getGlobalState("sortGridResults");
-
-    // get sortValue from droppableId
+    // derive sortValue from droppableId
     const replaceColumn = /column/gi;
     const replaceN = /N/gi;
-
     let sortValue1 = result.destination.droppableId;
     sortValue1 = sortValue1.replace(replaceColumn, "");
     sortValue1 = sortValue1.replace(replaceN, "-");
     const sortValue = parseInt(sortValue1, 10);
 
+    // assign key (like "s1") and value (sortValue like "-4")
     sortGridResults[result.draggableId] = sortValue;
     //   sortGridResults[result.draggableId] = result.destination.droppableId;
 
-    setGlobalState("sortGridResults", sortGridResults);
-    const testValue = getGlobalState("sortGridResults");
+    // create results string
+    const testForCompleteArray = Object.keys(sortGridResults);
+    if (testForCompleteArray.length === totalStatements) {
+      let resultsText = "";
+      for (let i = 0; i < totalStatements; i++) {
+        let key = `s${i + 1}`;
+        let newValue = sortGridResults[key];
+        resultsText += `${newValue}|`;
+      }
+      // remove trailing bar
+      if (resultsText.charAt(resultsText.length - 1) === "|") {
+        resultsText = resultsText.substr(0, resultsText.length - 1);
+      }
+      results.sort = resultsText;
+      setGlobalState("results", results);
+      console.log(resultsText);
+    }
 
-    console.log(JSON.stringify(testValue));
+    setGlobalState("sortGridResults", sortGridResults);
+    localStorage.setItem("sortGridResults", JSON.stringify(sortGridResults));
+
+    console.log(JSON.stringify(sortGridResults));
   } catch (error) {
     console.error(error);
   }
