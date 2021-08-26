60e0d01dc78665591fbe12c1075cd287c8f9dcb3
Shawn Banasick <banasick@gmail.com>
2021-08-26 10:58:11 +0900

    added statement numbers and lodash shuffle to cards

--- src/utilities/processStatementsXMLData.js
+++ src/utilities/processStatementsXMLData.js
@@ -1,10 +1,13 @@
 import setGlobalState from "../globalState/setGlobalState";
 import getGlobalState from "../globalState/getGlobalState";
+import shuffle from "lodash/shuffle";
 
 // prep column setup array
 const processStatementsXMLData = (dataObject) => {
+  const configObj = getGlobalState("configObj");
+  const shuffleCards = configObj.shuffleCards;
   const data = dataObject.statements.statement;
-  const statementsArray = [];
+  let statementsArray = [];
   const hasBeenLoaded = localStorage.getItem("hasBeenLoaded");
 
   if (!hasBeenLoaded) {
@@ -22,6 +25,11 @@ const processStatementsXMLData = (dataObject) => {
       tempObj.backgroundColor = "#e0e0e0";
       tempObj.statement = data[i]._text.trim();
       statementsArray.push(tempObj);
+    }
+
+    if (shuffleCards === true) {
+      const shuffledCards = shuffle(statementsArray);
+      statementsArray = [...shuffledCards];
     }
 
     const columnStatements = {};
