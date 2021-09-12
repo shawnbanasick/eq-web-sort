59241d88615cb6280171d10bf00aa0a1ed12506d
smb <smb>
2021-09-12 21:48:06 +0900

    fixed destination null error in calculateDragResults

--- src/pages/sort/calculateDragResults.js
+++ src/pages/sort/calculateDragResults.js
@@ -3,63 +3,66 @@ import setGlobalState from "../../globalState/setGlobalState";
 
 const calculateDragResults = (result) => {
   try {
-    const configObj = getGlobalState("configObj");
-    const results = getGlobalState("results");
-    const sortFinishedModalHasBeenShown = getGlobalState(
-      "sortFinishedModalHasBeenShown"
-    );
+    console.log(JSON.stringify(result));
+    if (result.destination !== null) {
+      const configObj = getGlobalState("configObj");
+      const results = getGlobalState("results");
+      const sortFinishedModalHasBeenShown = getGlobalState(
+        "sortFinishedModalHasBeenShown"
+      );
 
-    const totalStatements = configObj.totalStatements;
-    const sortGridResults = getGlobalState("sortGridResults");
+      const totalStatements = configObj.totalStatements;
+      const sortGridResults = getGlobalState("sortGridResults");
 
-    // derive sortValue from droppableId
-    const replaceColumn = /column/gi;
-    const replaceN = /N/gi;
-    let sortValue1 = result.destination.droppableId;
-    sortValue1 = sortValue1.replace(replaceColumn, "");
-    sortValue1 = sortValue1.replace(replaceN, "-");
-    const sortValue = parseInt(sortValue1, 10);
+      // derive sortValue from droppableId
+      const replaceColumn = /column/gi;
+      const replaceN = /N/gi;
+      let sortValue1 = result.destination.droppableId;
+      sortValue1 = sortValue1.replace(replaceColumn, "");
+      sortValue1 = sortValue1.replace(replaceN, "-");
+      const sortValue = parseInt(sortValue1, 10);
 
-    // assign key (like "s1") and value (sortValue like "-4")
-    sortGridResults[result.draggableId] = sortValue;
+      // assign key (like "s1") and value (sortValue like "-4")
+      sortGridResults[result.draggableId] = sortValue;
 
-    // create results string
-    const testForCompleteArray = Object.keys(sortGridResults);
-    let catchNan = false;
-    if (testForCompleteArray.length === totalStatements) {
-      let resultsText = "";
-      for (let i = 0; i < totalStatements; i++) {
-        let key = `s${i + 1}`;
-        let newValue = sortGridResults[key];
-        if (isNaN(newValue)) {
-          catchNan = true;
-        }
-        resultsText += `${newValue}|`;
-      } // loop end
+      // create results string
+      const testForCompleteArray = Object.keys(sortGridResults);
+      let catchNan = false;
+      if (testForCompleteArray.length === totalStatements) {
+        let resultsText = "";
+        for (let i = 0; i < totalStatements; i++) {
+          let key = `s${i + 1}`;
+          let newValue = sortGridResults[key];
+          if (isNaN(newValue)) {
+            catchNan = true;
+          }
+          resultsText += `${newValue}|`;
+        } // loop end
 
-      if (catchNan === true) {
-        // card in footer - sort not complete
-        setGlobalState("sortFinished", false);
-      } else {
-        // if sort is complete
-        // process string to remove trailing bar
-        setGlobalState("sortFinished", true);
+        if (catchNan === true) {
+          // card in footer - sort not complete
+          setGlobalState("sortFinished", false);
+        } else {
+          // if sort is complete
+          // process string to remove trailing bar
+          setGlobalState("sortFinished", true);
 
-        if (resultsText.charAt(resultsText.length - 1) === "|") {
-          resultsText = resultsText.substr(0, resultsText.length - 1);
-        }
+          if (resultsText.charAt(resultsText.length - 1) === "|") {
+            resultsText = resultsText.substr(0, resultsText.length - 1);
+          }
 
-        console.log(resultsText);
+          console.log(resultsText);
 
-        results.sort = resultsText;
-        setGlobalState("results", results);
-        if (sortFinishedModalHasBeenShown === false) {
-          setGlobalState("sortFinishedModalHasBeenShown", true);
-          setGlobalState("triggerSortingFinishedModal", true);
+          results.sort = resultsText;
+          setGlobalState("results", results);
+          if (sortFinishedModalHasBeenShown === false) {
+            setGlobalState("sortFinishedModalHasBeenShown", true);
+            setGlobalState("triggerSortingFinishedModal", true);
+          }
         }
       }
+      setGlobalState("sortGridResults", sortGridResults);
     }
-    setGlobalState("sortGridResults", sortGridResults);
   } catch (error) {
     console.error(error);
     console.log("there was an error in calculateDragResults");
