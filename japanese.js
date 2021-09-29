6d1ac82ec64b049268b064d14db6cda7a46772f6
Shawn Banasick <banasick@gmail.com>
2021-09-29 18:10:46 +0900

    added translations

---
+++ japanese.js
@@ -0,0 +1,71 @@
+{
+    "langLoginWelcomeText": `<center><h3>Welcome!</h3></center><br>このQメソドロジー研究プロジェクトに参加していただきありがとうございます。<br>以下のフォームで<strong>サインイン</strong>して、プロセスを開始してください。`,
+    "langLoginHeaderText": `サインイン`,
+    langLoginPartIdText: `お名前または参加者IDを入力してください`,
+    langAccessInputText: `このアンケートのアクセスコードを入力してください。`,
+    langPartIdWarning: `名前またはIDが無効`,
+    langAccessCodeWarning: `アクセスコードが無効です`,
+    loginSubmitButtonText: `提出`,
+    langLandingHelpModalHead: `Welcome!`,
+    langLandingHelpModalText: `このたびは、本学の研究プロジェクトにご参加いただきありがとうございます。サインインして、プロセスを開始してください。参加者IDやプロジェクトアクセスコードを送信すると、アンケートの説明が表示されます。よくお読みいただき、画面右下の「<strong>続ける</strong>」ボタンをクリックして次のステップにお進みください。<br><br> <center><iframe width="640" height="360"
+    src="https://www.youtube.com/embed/aHpeu5M7Rlo">
+    </iframe></center>`,
+
+
+
+    langtitleBarText: `予備的なソーティング`,
+    langPresortAgreement: `同意`,
+    langPresortStatements: `ステートメント`,
+    langPresortNeutral: `思い入れがない`,
+    langPresortDisagreement: `同意しない`,
+    langPresortModalHead: `ステップ1／4`,
+    langPresortModalText: `以下の文章をよく読んで、3つの山に分けてください。反対しがちな文章の山、賛成しがちなカードの山、残りの山です。<br><br>この説明書をもう一度読みたい場合は、左下にあるヘルプボタンをクリックしてください。`,
+    langPresortPreventNavModalHead: `ナビゲーションの無効化`,
+    langPresortPreventNavModalText: `予備的な仕分けを終えてから次のステップに進んでください。`,
+    langPresortFinishedModalHead: `   予備選別完了`,
+    langPresortFinishedModalText: `このステップは完了しました。ページ右下の「続ける」ボタンをクリックして次のステップにお進みください。`,
+
+    /*
+     ******** SORT  ****************** */
+    langSortHelpModalHead: `ステップ2／4`,
+    langSortHelpModalText: `このステップでは、ステートメントをグリッドパターンでソートしてください。あなたが最も同意するステートメントはグリッドの右端に、あなたが最も同意しないステートメントは左端に配置してください。このステップでのカードの並べ方については、ビデオをご覧ください。`,
+    langSortAgreement: `同意`,
+    langSortDisagreement: `同意しない`,
+    langCondOfInst: `今学期にiPadを使った経験に応じて、カードを分類してください。`,
+    langSortingCompleteModalHead: `ソート完了`,
+    langSortingCompleteModalText: `最初のソートが完了しました。<br><br>カードの配置を確認してください。<br><br>配置に問題がなければ、画面右下の<b>青色の「続行」ボタン</b>をクリックして次のステップに進んでください。`,
+    langSortPreventNavModalHead: `ナビゲーションの無効化`,
+    langSortPreventNavModalText: `グリッドソートを終えてから次のステップに進んでください。`,
+    langSortOverloadedColumnModalHead: `ナビゲーションの無効化`,
+    langSortOverloadedColumnModalText: `1つの列のカード枚数が多すぎます（オレンジ色の輪郭と破線の境界線が表示されます）。すべての列が正しい数になるように、カードの配置を調整してください。`,
+    /*
+     ******** POSTSORT  ****************** */
+    langPostsortHeader: `ソート後のコメント`,
+    langPostsortInstructions: `<h3>これらの記述について、あなたの考えをより詳しく述べてください。</h3>`,
+    langPlaceholder: `コメントを追加するにはここをクリック`,
+    langPostsortAgreement: `掲載されました: 同意`,
+    langPostsortDisagreement: `掲載されました: 同意しない`,
+    langPostsortModalHead: `ステップ3／4`,
+    langPostsortModalText: `ページ下部のボタンで、文字サイズやカードの高さを変更することができます`,
+
+    /*
+     ******** SURVEY  ****************** */
+    langSurveyHeader: `選別後のアンケート`,
+    langSurveyModalHead: `ステップ4／4`,
+    langSurveyModalText: `最後に、あなたの経歴に関する以下の質問にお答えください。タイトルに「*」がついている質問は必須です。`,
+    langSurveyPreventNavModalHead: `ナビゲーションの無効化`,
+    langSurveyPreventNavModalText: `次のステップに進む前に、必要な質問（赤枠の部分）にお答えください。`,
+
+    /*
+     ******** SUBMIT  ****************** */
+    langBtnTransfer: `データ提出`,
+    langTransferHead: `ファイナルステップ`,
+    langTransferTextAbove: `<h1>提出準備完了</h1><br><br> <h2>「データ送信」ボタンを押して、結果を送信してください。</h2>`,
+    langTransferTextBelow: `送信確認メッセージが表示されない場合は、<br>数分待ってから再度送信ボタンをクリックしてください。`,
+    langTransferOkModalHeader: `成功 - データがサーバーに送信されました。`,
+    langTransferOkModalText: `Easy HTMLQをご利用いただきありがとうございます！<br><br>これでブラウザのタブを閉じることができます。`,
+    langTransferFailModalHeader: `サーバーへの接続に失敗`,
+    langTransferFailModalText: `しばらくしてからもう一度お試しください。`,
+    langFallbackMessage: `データ送信に失敗しました。結果をダウンロードして、添付ファイルとして ken.q.tools@gmail.com に送信してください。 ありがとうございました。`,
+    langBtnDownload: `ダウンロード`,
+}
