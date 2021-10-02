cc9a5548326ff155296aec08e073a2fee5f8a9c3
smb <smb>
2021-10-02 14:01:37 +0900

    adjusted number input

--- src/pages/presort/PresortDND.jsx
+++ src/pages/presort/PresortDND.jsx
@@ -25,124 +25,127 @@ function PresortDND(props) {
 
   const cardHeight = 210;
 
-  const onDragEnd = useCallback((result, columns, setColumns) => {
-    if (!result.destination) return;
-    const { source, destination } = result;
+  const onDragEnd = useCallback(
+    (result, columns, setColumns) => {
+      if (!result.destination) return;
+      const { source, destination } = result;
 
-    // update statement characteristics
-    const columnStatements = getGlobalState("columnStatements");
-    const statementsArray = [...columnStatements.statementList];
-    const destinationId = result.destination.droppableId;
-    const draggableId = result.draggableId;
+      // update statement characteristics
+      const columnStatements = getGlobalState("columnStatements");
+      const statementsArray = [...columnStatements.statementList];
+      const destinationId = result.destination.droppableId;
+      const draggableId = result.draggableId;
 
-    for (let i = 0; i < statementsArray.length; i++) {
-      if (statementsArray[i].id === draggableId) {
-        if (destinationId === "neg") {
-          statementsArray[i].divColor = "isNegativeStatement";
-          statementsArray[i].cardColor = "pinkSortCard";
-          statementsArray[i].pinkChecked = true;
-          statementsArray[i].yellowChecked = false;
-          statementsArray[i].greenChecked = false;
-          statementsArray[i].sortValue = 111;
+      for (let i = 0; i < statementsArray.length; i++) {
+        if (statementsArray[i].id === draggableId) {
+          if (destinationId === "neg") {
+            statementsArray[i].divColor = "isNegativeStatement";
+            statementsArray[i].cardColor = "pinkSortCard";
+            statementsArray[i].pinkChecked = true;
+            statementsArray[i].yellowChecked = false;
+            statementsArray[i].greenChecked = false;
+            statementsArray[i].sortValue = 111;
+          }
+          if (destinationId === "neutral") {
+            statementsArray[i].divColor = "isUncertainStatement";
+            statementsArray[i].cardColor = "yellowSortCard";
+            statementsArray[i].pinkChecked = false;
+            statementsArray[i].yellowChecked = true;
+            statementsArray[i].greenChecked = false;
+            statementsArray[i].sortValue = 222;
+          }
+          if (destinationId === "pos") {
+            statementsArray[i].divColor = "isPositiveStatement";
+            statementsArray[i].cardColor = "greenSortCard";
+            statementsArray[i].pinkChecked = false;
+            statementsArray[i].yellowChecked = false;
+            statementsArray[i].greenChecked = true;
+            statementsArray[i].sortValue = 333;
+          }
         }
-        if (destinationId === "neutral") {
-          statementsArray[i].divColor = "isUncertainStatement";
-          statementsArray[i].cardColor = "yellowSortCard";
-          statementsArray[i].pinkChecked = false;
-          statementsArray[i].yellowChecked = true;
-          statementsArray[i].greenChecked = false;
-          statementsArray[i].sortValue = 222;
+      }
+
+      // set new ordering
+      for (let i = 0; i < statementsArray.length; i++) {
+        statementsArray[i].listIndex = i + 1;
+      }
+
+      // save to memory
+      columnStatements.statementList = [...statementsArray];
+      setGlobalState("columnStatements", columnStatements);
+
+      // when dropped on different droppable
+      if (source.droppableId !== destination.droppableId) {
+        const sourceColumn = columns[source.droppableId];
+        const destColumn = columns[destination.droppableId];
+        const sourceItems = [...sourceColumn.items];
+        const destItems = [...destColumn.items];
+        const [removed] = sourceItems.splice(source.index, 1);
+
+        // change background color
+        if (destColumn.id === "pos") {
+          removed.backgroundColor = configObj.greenCardColor;
         }
-        if (destinationId === "pos") {
-          statementsArray[i].divColor = "isPositiveStatement";
-          statementsArray[i].cardColor = "greenSortCard";
-          statementsArray[i].pinkChecked = false;
-          statementsArray[i].yellowChecked = false;
-          statementsArray[i].greenChecked = true;
-          statementsArray[i].sortValue = 333;
+        if (destColumn.id === "neg") {
+          removed.backgroundColor = configObj.pinkCardColor;
         }
+        if (destColumn.id === "neutral") {
+          removed.backgroundColor = configObj.yellowCardColor;
+        }
+
+        destItems.splice(destination.index, 0, removed);
+
+        // calc remaining statements
+        if (sourceColumn.id === "cards") {
+          presortSortedStatementsNum =
+            configObj.totalStatements - sourceColumn.items.length + 1;
+          setGlobalState(
+            "presortSortedStatementsNum",
+            presortSortedStatementsNum
+          );
+          presortSortedStatementsNum = presortSortedStatementsNum.toString();
+
+          setGlobalState(
+            "presortSortedStatementsNum",
+            presortSortedStatementsNum
+          );
+        }
+
+        // update progress bar
+        const sortedStatements = +getGlobalState("presortSortedStatementsNum");
+        const ratio = sortedStatements / configObj.totalStatements;
+        const completedPercent = (ratio * 30).toFixed();
+        // update Progress Bar State
+        setGlobalState("progressScoreAdditional", completedPercent);
+
+        // update columns
+        setColumns({
+          ...columns,
+          [source.droppableId]: {
+            ...sourceColumn,
+            items: sourceItems,
+          },
+          [destination.droppableId]: {
+            ...destColumn,
+            items: destItems,
+          },
+        });
+      } else {
+        const column = columns[source.droppableId];
+        const copiedItems = [...column.items];
+        const [removed] = copiedItems.splice(source.index, 1);
+        copiedItems.splice(destination.index, 0, removed);
+        setColumns({
+          ...columns,
+          [source.droppableId]: {
+            ...column,
+            items: copiedItems,
+          },
+        });
       }
-    }
-
-    // set new ordering
-    for (let i = 0; i < statementsArray.length; i++) {
-      statementsArray[i].listIndex = i + 1;
-    }
-
-    // save to memory
-    columnStatements.statementList = [...statementsArray];
-    setGlobalState("columnStatements", columnStatements);
-
-    // when dropped on different droppable
-    if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = columns[source.droppableId];
-      const destColumn = columns[destination.droppableId];
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
-      const [removed] = sourceItems.splice(source.index, 1);
-
-      // change background color
-      if (destColumn.id === "pos") {
-        removed.backgroundColor = configObj.greenCardColor;
-      }
-      if (destColumn.id === "neg") {
-        removed.backgroundColor = configObj.pinkCardColor;
-      }
-      if (destColumn.id === "neutral") {
-        removed.backgroundColor = configObj.yellowCardColor;
-      }
-
-      destItems.splice(destination.index, 0, removed);
-
-      // calc remaining statements
-      if (sourceColumn.id === "cards") {
-        presortSortedStatementsNum =
-          configObj.totalStatements - sourceColumn.items.length + 1;
-        setGlobalState(
-          "presortSortedStatementsNum",
-          presortSortedStatementsNum
-        );
-        presortSortedStatementsNum = presortSortedStatementsNum.toString();
-
-        setGlobalState(
-          "presortSortedStatementsNum",
-          presortSortedStatementsNum
-        );
-      }
-
-      // update progress bar
-      const sortedStatements = +getGlobalState("presortSortedStatementsNum");
-      const ratio = sortedStatements / configObj.totalStatements;
-      const completedPercent = (ratio * 30).toFixed();
-      // update Progress Bar State
-      setGlobalState("progressScoreAdditional", completedPercent);
-
-      // update columns
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems,
-        },
-      });
-    } else {
-      const column = columns[source.droppableId];
-      const copiedItems = [...column.items];
-      const [removed] = copiedItems.splice(source.index, 1);
-      copiedItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...column,
-          items: copiedItems,
-        },
-      });
-    }
-  }); // END DRAG-END
+    },
+    [presortSortedStatementsNum]
+  ); // END DRAG-END
 
   const [columns, setColumns] = useState({
     cards: {
